dc2365f845de4e1ee30eeb53dbc8eac2d39c6968
Antony <A.Krutenok@gmail.com>
2019-08-07 14:19:19 +0300

    feat: change statuses on 'run'; styles for completed tests

--- app.js
+++ app.js
@@ -1,16 +1,25 @@
-angular.module('tests', [])
-  .controller('TestsList', function() {
-    const tests = this;
+angular
+  .module('tests', [])
+  .controller('TestsList', mainCtrl);
 
-    tests.list = [
-      { title: 'Test #1' },
-      { title: 'Test #2' },
-      { title: 'Test #3' },
-      { title: 'Test #4' },
-      { title: 'Test #5' },
-      { title: 'Test #6' }];
- 
-    tests.runAll = function() {
-      tests.list.forEach(test => test.function())
-    };
-  });
+function mainCtrl() {
+  this.list = [
+    { title: 'Test #1', inProgress: false, result: false, reason: '', function: () => this.changeResult(0, { success: true, descr: '' }) },
+    { title: 'Test #2', inProgress: false, result: true, reason: '', function: () => this.changeResult(1, { success: true, descr: '' }) },
+    { title: 'Test #3', inProgress: true, result: '', reason: '', function: () => this.changeResult(2, { success: false, descr: 'error' }) },
+    { title: 'Test #4', inProgress: true, result: '', reason: '', function: () => () => this.changeResult(3, { success: true, descr: '' }) },
+    { title: 'Test #5', inProgress: true, result: '', reason: '', function: () => () => this.changeResult(4, { success: false, descr: 'another error' }) },
+    { title: 'Test #6', inProgress: true, result: '', reason: '', function: () => () => this.changeResult(5, { success: true, descr: '' }) }];
+
+  this.changeResult = (id, result) => {
+    this.list[id].inProgress = false;
+    this.list[id].result = result.success;
+    this.list[id].reason = result.descr;
+  }
+
+  this.runAll = () => {
+    this.list.forEach(test => test.function())
+  };
+
+
+}
