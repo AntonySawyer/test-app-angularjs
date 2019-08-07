7f052ad9fb08e0eb7a225cdb9b856d0488fbd778
Antony <A.Krutenok@gmail.com>
2019-08-07 14:24:46 +0300

    hotfix: typo in arrow functions 4-6

--- app.js
+++ app.js
@@ -4,12 +4,12 @@ angular
 
 function mainCtrl() {
   this.list = [
-    { title: 'Test #1', inProgress: false, result: false, reason: '', function: () => this.changeResult(0, { success: true, descr: '' }) },
-    { title: 'Test #2', inProgress: false, result: true, reason: '', function: () => this.changeResult(1, { success: true, descr: '' }) },
-    { title: 'Test #3', inProgress: true, result: '', reason: '', function: () => this.changeResult(2, { success: false, descr: 'error' }) },
-    { title: 'Test #4', inProgress: true, result: '', reason: '', function: () => () => this.changeResult(3, { success: true, descr: '' }) },
-    { title: 'Test #5', inProgress: true, result: '', reason: '', function: () => () => this.changeResult(4, { success: false, descr: 'another error' }) },
-    { title: 'Test #6', inProgress: true, result: '', reason: '', function: () => () => this.changeResult(5, { success: true, descr: '' }) }];
+    { title: 'Test #1', inProgress: '', result: '', reason: '', function: () => this.changeResult(0, { success: true, descr: '' }) },
+    { title: 'Test #2', inProgress: '', result: '', reason: '', function: () => this.changeResult(1, { success: true, descr: '' }) },
+    { title: 'Test #3', inProgress: '', result: '', reason: '', function: () => this.changeResult(2, { success: false, descr: 'error' }) },
+    { title: 'Test #4', inProgress: '', result: '', reason: '', function: () => this.changeResult(3, { success: true, descr: '' }) },
+    { title: 'Test #5', inProgress: '', result: '', reason: '', function: () => this.changeResult(4, { success: false, descr: 'another error' }) },
+    { title: 'Test #6', inProgress: '', result: '', reason: '', function: () => this.changeResult(5, { success: true, descr: '' }) }];
 
   this.changeResult = (id, result) => {
     this.list[id].inProgress = false;
@@ -18,7 +18,8 @@ function mainCtrl() {
   }
 
   this.runAll = () => {
-    this.list.forEach(test => test.function())
+    this.list.map(test => test.inProgress = true);
+    this.list.forEach(test => test.function());
   };
 
 
